9f938b14f476be458a24b1988c45904d5833dc3a
philipjhj <philipjhj@gmail.com>
2017-10-20 13:59:18 +0200

    implements multiple mirror functionality

--- setupRemoteMirror.py
+++ setupRemoteMirror.py
@@ -3,11 +3,14 @@ import git
 import re
 from shutil import copyfile
 
-organizationName = 'DTU-DABAI'  # DTUComputeCognitiveSystems
+organizations = ['DTU-DABAI', 'DTUComputeCognitiveSystems']
 
 repo = git.Repo('.')
 assert not repo.bare
 repoGit = repo.git
+
+# for remoteName in repo.remotes:
+#    print(remoteName)
 
 # Get name of repository from the origin
 urlOrigin = repoGit.remote('get-url', 'origin')
@@ -16,11 +19,11 @@ repoName = re.match(pattern, urlOrigin)
 repoName = repoName.group(1)
 # print(repoName)
 
-# Set URL for mirror
-remoteMirrorURL = 'https://github.com/' + organizationName + '/' + repoName
-
-# Add mirror as remote to local repository
-repoGit.remote('add', '--mirror=push', 'mirror_repo', remoteMirrorURL)
-
+for organizationName in organizations:
+    # Set URL for mirror
+    remoteMirrorURL = 'https://github.com/' + organizationName + '/' + repoName
+    # Add mirror as remote to local repository
+    repoGit.remote('add', '--mirror=push', 'mirror_repo_' + organizationName,
+                   remoteMirrorURL)
 # Install pre-push hook
 copyfile('mirrorSetup/hooks/pre-push-python', '.git/hooks/pre-push')
